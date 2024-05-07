ae1c073cc7b8cf81139742789825f4b20286a9a1
Monika Januszek <monika.januszek.mail@gmail.com>
2024-05-07 17:35:21 +0200

    dices styling added

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,26 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="container">
       <h1>Choose your dice</h1>
-    </>
+      <div className="dices">
+        <img
+          className="dice"
+          src="src/assets/11121422_fi_rr_dice_d6_icon.png"
+          alt=""
+        />
+        <img
+          className="dice"
+          src="src/assets/11121534_fi_rr_dice_d10_icon.png"
+          alt=""
+        />
+        <img
+          className="dice"
+          src="src/assets/8665279_dice_d20_icon.png"
+          alt=""
+        />
+      </div>
+    </div>
   );
 }
 
